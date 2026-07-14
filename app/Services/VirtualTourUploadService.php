0a47e8922b5febcb6250690f0bd0beae9f6041d7
ghilmanfz <ghilmanfz@gmail.com>
2026-07-14 18:28:54 +0700

    feat: Increase maximum upload size to 80MB for virtual tour files

--- app/Services/VirtualTourUploadService.php
+++ app/Services/VirtualTourUploadService.php
@@ -15,7 +15,7 @@ use ZipArchive;
 
 class VirtualTourUploadService
 {
-    public const MAX_UPLOAD_KB = 40960;
+    public const MAX_UPLOAD_KB = 81920;
 
     public const SUPPORTED_EXTENSIONS = ['zip', 'rar', 'png', 'jpg', 'jpeg', 'webp'];
 
